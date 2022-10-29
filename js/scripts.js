68465dbd023184fa33a48b703ac683d4865fa08c
Blooife <beusas.23@gmail.com>
2022-10-29 18:16:10 +0300

    finish line

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
- 
-    // Кнопка по которой происходит клик
-    let callBackButton = document.getElementById('reserve-button');
-   
-    // Модальное окно, которое необходимо открыть
-    let modal1 = document.getElementById('modal-1');
-   
-    // Кнопка "закрыть" внутри модального окна
-    let closeButton = modal1.getElementsByClassName('modal__close-button')[0];
-   
-    callBackButton.onclick = function () {
-      modal1.classList.add('modal_active');
+let btn = document.querySelector('#btn');
+btn.setAttribute('disabled',true);
+    function validate_form ( )
+{    
+   // let btn = document.querySelector('#btn');
+    let username = document.querySelector('#username'); 
+    let data = document.querySelector('#date'); 
+    let email = document.querySelector('#email'); 
+    let table = document.querySelector('#number');
+     if(username.value.length < 1 || email.value.length <1 || data.value.length<1 || table.selectedIndex < 1){
+      btn.setAttribute('disabled',true);
+    } else{
+      btn.removeAttribute('disabled');
     }
-   
-    closeButton.onclick = function () {
-      modal1.classList.remove('modal_active');
-    }
-  });
+}
+
+let modal1 = document.querySelector('#modal-1');
+let closeButton = document.querySelector('#modal-close');
+btn.onclick = function () {
+  modal1.classList.add('modal_active');
+  //console.log('Клик!');
+}
+
+closeButton.onclick = function () {
+  modal1.classList.remove('modal_active');
+  //console.log('Клик2!');
+  location.reload();
+} 
